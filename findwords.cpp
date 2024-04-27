80d6405a3850146c5fe45f0f3bc9a7b0a72c46fb
akmalfadhrhmn <144765179+akmalfadhrhmn@users.noreply.github.com>
2024-04-27 13:43:02 +0700

    Update findwords.cpp

--- findwords.cpp
+++ findwords.cpp
@@ -8,9 +8,9 @@ bool FindWord(char matrik[m][m], string kata, int baris, int kolom){
             c=c-'A'+'a';
         }
     }
-    for (int i=0; i<baris; i++){
-        for (int j=0; j <= kolom-kata.length(); j++){
-            string barisStr(matrik[i]+j, matrik[i]+j+kata.length());
+    for (int i = 0; i < baris; i++){
+        for (int j = 0; j <= kolom-kata.length(); j++){
+            string barisStr(matrik[i] + j, matrik[i] + j + kata.length());
         
 
 int main()
