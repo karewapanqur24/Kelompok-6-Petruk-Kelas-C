c3fae096a9536ffa5e2207b0793a491b8f57f4e3
Irpan Purnama <109006679+karewapanqur24@users.noreply.github.com>
2024-04-27 14:07:09 +0700

    Membuat fungsi utama
    -Untuk memasukkan nilai integer (sebagai jumlah kata/huruf yang ingin dicari).
    -Membuat loop untuk memasukkan data tipe string (huruf yang akan dicari berdasarkan banyaknya inputan integer).
    -Mendeklarasikan array dua dimensi dengan tipe data char.

--- findwords.cpp
+++ findwords.cpp
@@ -15,6 +15,19 @@ bool FindWord(char matrik[m][m], string kata, int baris, int kolom){
 
 int main()
 {
-    cout << "Kelompok 6 Petruk";
-    return 0;
+    int caribrp;
+    cin >> caribrp;
+    string katakey[caribrp];
+    for (int i = 0; i < caribrp; i++)
+    {
+        cin >> katakey[i];
+    }
+    char matrik[m][m] = {
+        {'A', 'A', 'F', 'L', 'K', 'H', 'P', 'F', 'S', 'S', 'U', 'F', 'I', 'C', 'I', 'C', 'L', 'E', 'S', 'G', 'N', 'N', 'H'},
+        {'S', 'F', 'V', 'R', 'E', 'O', 'M', 'R', 'W', 'L', 'R', 'T', 'T', 'S', 'X', 'O', 'Q', 'Q', 'N', 'A', 'O', 'A', 'O'},
+        {'Q', 'E', 'I', 'A', 'I', 'F', 'X', 'A', 'E', 'I', 'R', 'F', 'V', 'F', 'Y', 'S', 'X', 'I', 'M', 'I', 'N', 'J', 'I'},
+        {'W', 'S', 'T', 'R', 'L', 'G', 'O', 'C', 'A', 'P', 'B', 'I', 'A', 'F', 'I', 'W', 'I', 'W', 'T', 'U', 'A', 'C', 'M'},
+        {'F', 'E', 'Y', 'A', 'E', 'A', 'I', 'S', 'T', 'P', 'C', 'R', 'L', 'U', 'J', 'K', 'O', 'A', 'K', 'C', 'E', 'R', 'S'},
+        {'R', 'V', 'D', 'A', 'K', 'P', 'N', 'D', 'E', 'E', 'H', 'D', 'E', 'M', 'S', 'N', 'C', 'K', 'K', 'F', 'O', 'A', 'H'},
+        {'M', 'R', 'N', 'E', 'D', 'S', 'L', 'C', 'R', 'R', 'I', 'W', 'N', 'R', 'S', 'A', 'A', 'F', 'I', 'T', 'M', 'M', 'I'},
 }
