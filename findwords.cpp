d793b622c8acfa117aa5dfdbd247f9e73ff45205
akmalfadhrhmn <144765179+akmalfadhrhmn@users.noreply.github.com>
2024-04-27 11:00:18 +0700

    findwords.cpp

--- findwords.cpp
+++ findwords.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
+const int m=23;
+bool FindWord(char matrik[m][m], string kata, int baris, int kolom){
+    for (char& c : kata){
+        if (c >= 'A' && c <= 'Z'){
+            c=c-'A'+'a';
+        }
+    }
+    for (int i=0; i<baris; i++){
+        for (int j=0; j <= kolom-kata.length(); j++){
+            string barisStr(matrik[i]+j, matrik[i]+j+kata.length());
+        
 
 int main()
 {
